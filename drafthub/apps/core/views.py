5acf3ae917b06eb2d14f1303b93b76a1e6aa55d4
Felipe Lincoln <felipelincoln@usp.br>
2020-03-30 16:18:52 -0300

    implement logout

--- drafthub/apps/core/views.py
+++ drafthub/apps/core/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import logout # provisional
+from django.shortcuts import redirect # provisional
 
-class Home(TemplateView):
+class HomeView(TemplateView):
     template_name = 'core/home.html'
+
+
+def logout_view(request): # provisional
+    logout(request)
+    return redirect('home')
--- drafthub/urls.py
+++ drafthub/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from drafthub.apps.core.views import Home # provisional
+from drafthub.apps.core.views import HomeView, logout_view # provisional
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('social_django.urls'), name='social'),
-    path('', Home.as_view(), name='home'), # provisional
+    path('', HomeView.as_view(), name='home'), # provisional
+    path('logout/', logout_view, name='logout'), # provisional
 ]

ef0c17716b29e38b5ea6d7d62901b0a5756cbec6
Felipe Lincoln <felipelincoln@usp.br>
2020-04-16 00:51:15 -0300

    fix relative link issues

--- drafthub/core/templatetags/drafthub_extras.py
+++ drafthub/core/templatetags/drafthub_extras.py
@@ -2,26 +2,30 @@ import re
 import requests
 from django import template
 from django.utils.html import escape
-from django.utils.safestring import mark_safe 
+from django.utils.safestring import mark_safe
 import markdown as pymd
 
-register = template.Library()
 
+register = template.Library()
 
 @register.filter
 @mark_safe
 def markdown(instance):
-    regex_expr = '^https:\/\/github\.com\/(.+?)\/(.+?)\/blob\/(.*\.md)$'
-    regex_tester = re.compile(regex_expr)
-    regex_results = regex_tester.match(instance.github_url)
+    url_origin = instance.github_url.rsplit('/', 1)[0] + '/'
+    url_noblob = instance.github_url.replace('blob/', '')
+    url_stripped = url_noblob.lstrip('https://github.com/')
+    url_raw = 'https://raw.githubusercontent.com/' + url_stripped
 
-    handle, repo, md = regex_results.groups()
-    raw_github_url = f'https://raw.githubusercontent.com/{handle}/{repo}/{md}'
-
-    content = requests.get(raw_github_url)
+    content = requests.get(url_raw)
     content = content.text
-    content = escape(content)
-    return pymd.markdown(content)
+    
+    re_links = '\[(.*)\]\((?!http)(.+)\)'
+    match_links = re.compile(re_links)
+    content_transform = match_links.sub(
+        r'[\1](' + url_origin + r'\2)', content)
+
+    content_esc = escape(content_transform)
+    return pymd.markdown(content_esc)
 
 
 @register.filter

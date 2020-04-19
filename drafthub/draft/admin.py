b1cf458077e3db41ffe183be12159e1046570cf1
Felipe Lincoln <felipelincoln@usp.br>
2020-04-18 23:10:59 -0300

    add last_update field to draft model

--- drafthub/draft/admin.py
+++ drafthub/draft/admin.py
@@ -4,8 +4,8 @@ from .models import Draft
 
 @admin.register(Draft)
 class PostAdmin(admin.ModelAdmin):
-    fields = ('blog', 'github_url', 'title', 'abstract', 'slug', 'pub_date',)
-    readonly_fields = ('slug', 'pub_date',)
+    fields = ('blog', 'github_url', 'title', 'abstract', 'slug', 'pub_date', 'last_update')
+    readonly_fields = ('slug', 'pub_date', 'last_update')
 
-    list_display = ('blog', 'title', 'slug', 'pub_date',)
-    list_filter = ('blog', 'pub_date',)
+    list_display = ('blog', 'title', 'slug', 'pub_date', 'last_update')
+    list_filter = ('blog', 'pub_date', 'last_update')

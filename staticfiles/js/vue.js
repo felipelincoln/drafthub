5502fee2bc33bcba7b26630561012f6ea9f382a2
Felipe Lincoln <felipelincoln@usp.br>
2020-05-26 17:44:13 -0300

    update vue.js file

--- staticfiles/js/vue.js
+++ staticfiles/js/vue.js
@@ -1,17 +1,15 @@
 // vue.js
 Vue.component('draft-tag', {
 	props: [
-		'tagName',
 		'tagUrl',
 		'tagPack',
 		'tagIcon',
 	],
-	
 		template: `
 			<a :href="tagUrl">
 				<b-tag type="is-dark" size="is-medium">
-					<b-icon v-if="tagIcon != 'None'" :pack="tagPack" :icon="tagIcon"></b-icon>
-					<span>{{ tagName }}</span>
+					<b-icon v-if="tagIcon != 'None'" :pack="tagPack" :icon="tagIcon" size="is-small"></b-icon>
+					<span><slot></slot></span>
 				</b-tag>
 			</a>
 		`

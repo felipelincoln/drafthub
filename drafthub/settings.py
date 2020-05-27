9eaedd7b5037e2eb5cad33a11caa27209a09d912
Felipe Lincoln <felipelincoln@usp.br>
2020-05-27 01:48:29 -0300

    revert statifiles changes

--- drafthub/settings.py
+++ drafthub/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # dev
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # prod
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'

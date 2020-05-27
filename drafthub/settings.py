695f556578345e89163290e8d8c7a422bb2ec4f7
Felipe Lincoln <felipelincoln@usp.br>
2020-05-27 01:38:34 -0300

    remore staticfiles_dirs

--- drafthub/settings.py
+++ drafthub/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # dev
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # prod
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'

08dac54a5563d8b7dbcd74fcc7969cb713bd2241
Felipe Lincoln <felipelincoln@usp.br>
2020-05-27 01:33:22 -0300

    revert changes

--- drafthub/settings.py
+++ drafthub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     # third parties
@@ -135,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # dev
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # prod
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Social auth
